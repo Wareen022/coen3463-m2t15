f4c78ed5af95a4b6c29e7ab18d9df44e946a78d7
Warren Bernabe <ren22430@gmail.com>
2017-01-03 16:20:47 +0800

    Nodemailer

--- routes/ContactUs.js
+++ routes/ContactUs.js
@@ -11,14 +11,14 @@ router.post('/send', function(req, res, next){
 	var trasporter = nodemailer.createTransport({
 		service: 'Gmail',
 		auth: {
-			user: 'TheEventMailSample@gmail.com',
-			pass: 'TheEventMailSample123456',
+			user: 'ren22430@gmail.com',
+			pass: 'brnb04022030',
 		}
 	});
 
 	var mailOptions = {
-		from: 'name <name@outlook.com>',
-		to: 'TheEventMailSample@gmail.com',
+		from: 'name <ren22430@gmail.com>',
+		to: 'ren22430@gmail.com',
 		subject: 'inquiries',
 		text: 'you have a new message... Name: '+req.body.name+ ' Email: '+req.body.email+ ' Message: '+req.body.Message,
 		html: '<p>You got a new message with the following details..</p><ul><li>Name: '+req.body.name+ '</li><li>Email: '+req.body.email+ '</li><li>Message: '+req.body.message+ '</li></ul>'
