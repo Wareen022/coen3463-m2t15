8db1e799fe44478722d5d6f3284e136e11cb1e34
ren bernabe <ren bernabe>
2017-02-08 10:33:01 +0800

    Transporter

--- routes/ContactUs.js
+++ routes/ContactUs.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/send', function(req, res, next){
-	var trasporter = nodemailer.createTransport({
+	var transporter = nodemailer.createTransport({
 		service: 'Gmail',
 		auth: {
 			user: 'coen3463t15@gmail.com',
@@ -23,7 +23,7 @@ router.post('/send', function(req, res, next){
 		text: 'you have a new message... Name: '+req.body.name+ ' Email: '+req.body.email+ ' Message: '+req.body.Message,
 		html: '<p>You got a new message with the following details..</p><ul><li>Name: '+req.body.name+ '</li><li>Email: '+req.body.email+ '</li><li>Message: '+req.body.message+ '</li></ul>'
 	};
-	trasporter.sendMail(mailOptions, function(error, info){
+	transporter.sendMail(mailOptions, function(error, info){
 		if (error){
 			console.log(error);
 			res.redirect('/');
