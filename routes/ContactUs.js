8c685f844d2b1aa1a5360fb8c450c2f7c0948adb
ren bernabe <ren bernabe>
2017-02-12 12:41:46 +0800

    modified contactus email

--- routes/ContactUs.js
+++ routes/ContactUs.js
@@ -11,14 +11,14 @@ router.post('/send', function(req, res, next){
     var transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: 'coen3463t15@gmail.com',
-            pass: 'p@ssw0rd12345',
+            user: 'coen3463@gmail.com',
+            pass: 'password1234567890',
         }
     });
 
     var mailOptions = {
-        from: 'JillyBeans <coen3463t15@gmail.com>',
-        to: 'coen3463t15@gmail.com',
+        from: 'JillyBeans <coen3463@gmail.com>',
+        to: 'coen3463@gmail.com',
         subject: 'inquiries',
         text: 'you have a new message... Name: '+req.body.name+ ' Email: '+req.body.email+ ' Message: '+req.body.message,
         html: '<p>You got a new message with the following details..</p><ul><li>Name: '+req.body.name+ '</li><li>Email: '+req.body.email+ '</li><li>Message: '+req.body.message+ '</li></ul>'
